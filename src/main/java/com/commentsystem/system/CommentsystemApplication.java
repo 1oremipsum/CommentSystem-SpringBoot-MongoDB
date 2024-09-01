edf1648c363fcd617d1563ab23dc337a72fcdf8e
Allan San <allanterzisanches@gmail.com>
2024-09-01 16:32:51 -0300

    Initial

---
+++ src/main/java/com/commentsystem/system/CommentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.commentsystem.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commentsystem/system/CommentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commentsystem.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
