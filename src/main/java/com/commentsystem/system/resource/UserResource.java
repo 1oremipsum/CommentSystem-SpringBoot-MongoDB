d24cd15bbd555ed08e008679b3daec01fcc1273d
Allan San <allanterzisanches@gmail.com>
2024-09-28 21:13:59 -0300

    User deletion by id

--- src/main/java/com/commentsystem/system/resource/UserResource.java
+++ src/main/java/com/commentsystem/system/resource/UserResource.java
@@ -6,17 +6,15 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.commentsystem.system.domain.User;
 import com.commentsystem.system.dto.UserDTO;
 import com.commentsystem.system.service.UserService;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @RestController
@@ -37,13 +35,19 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
        User obj = service.findById(id);
        return ResponseEntity.ok().body(new UserDTO(obj));
-   }
+    }
 
-   @RequestMapping(method=RequestMethod.POST)
-   public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+    @PostMapping(value = "/")
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
       User obj = service.fromDTO(objDto);
       obj = service.insert(obj);
       URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
       return ResponseEntity.created(uri).build();
-  }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
